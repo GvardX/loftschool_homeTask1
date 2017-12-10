466b0a232b06c311679b76096afa00d14acb8d25
Andrey Azimov <gvardx@gmail.com>
2017-12-10 14:12:29 +0300

    сборка готова и  первая страница

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 const gulp = require("gulp"),
+      del = require("del"),
+      browserSync = require("browser-sync").create(),
+      webpack = require("webpack"),
+      webpackConfig = require("./webpack.config.js"),
+      gulpWebpack = require('gulp-webpack'),
       gp = require("gulp-load-plugins")();
     
 const paths = {
@@ -27,17 +32,68 @@ const paths = {
 
 function templates() {
     return gulp.src(paths.teamplates.pages)
+    .pipe(gp.plumber())
     .pipe(gp.pug({pretty: true}))
     .pipe(gulp.dest(paths.root));
 }
 
 function styles() {
     return gulp.src('./src/styles/app.scss')
+        .pipe(gp.plumber())
         .pipe(gp.sourcemaps.init())
         .pipe(gp.sass({outputStyle: 'compressed'}))
+        .pipe(gp.autoprefixer())
         .pipe(gp.sourcemaps.write())
         .pipe(gp.rename({suffix: '.min'}))
         .pipe(gulp.dest(paths.styles.dist))
 }
+
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe(gp.imagemin())
+        .pipe(gulp.dest(paths.images.dist))
+}
+
+function scripts() {
+    return gulp.src("./src/scripts/app.js")
+        .pipe(gulpWebpack(webpackConfig, webpack))
+        .pipe(gulp.dest(paths.scripts.dist))
+}
+
+function fonts() {
+    return gulp.src(paths.fonts.src)
+
+        .pipe(gulp.dest(paths.fonts.dist))
+}
+
+function clean() {
+    return del(paths.root);
+}
+
+function watch() {
+    gulp.watch(paths.teamplates.src, templates);
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.images.src, images);
+    gulp.watch(paths.fonts.src, fonts);
+    gulp.watch(paths.scripts.src, scripts);
+}
+
+
+function server() {
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+
 exports.templates = templates;
 exports.styles = styles;
+exports.images = images;
+exports.fonts = fonts;
+exports.clean = clean;
+
+gulp.task('default', gulp.series(
+    clean,
+    gulp.parallel(styles, templates, images, fonts, scripts),
+    gulp.parallel(watch, server)
+));
