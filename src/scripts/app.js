7350e39bbd2cccd87cf904c69ce4337fd5b4e058
Andrey Azimov <gvardx@gmail.com>
2017-12-25 00:12:21 +0300

    адаптив закончен, с кодом js проблемы

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,23 +1,47 @@
-var parallaxContainer = document.getElementById('parallax'),
-  layers = parallaxContainer.children;
+import {parallaxWelcome} from './common/parallax_welcome';
 
-var moveLayers = function (e) {
-  var initialX = (window.innerWidth / 2) - e.pageX;
-  var initialY = (window.innerHeight / 2) - e.pageY;
+var $ = require('jquery');
+var blur = (function(){
+  var wrapper = document.querySelector('.form__connect'),// blur не знаю почему не работает
+    connectBg = document.querySelector('.connect_bg');
 
-  [].slice.call(layers).forEach(function(layer, index) {
-    var 
-      divider = index / 80,
-      positionX = initialX * divider,
-      positionY = initialY * divider,
-      bottomPosition = (window.innerHeight / 2) * divider,
-      transformString = 'translate(' + positionX + 'px,' + positionY + 'px)',
-      image = layer.firstElementChild;
+  return {
+    set: function() {
+      var imgWidth =  document.querySelector('.form').offsetWidth,
+        posLeft = -wrapper.offsetLeft,
+        posTop =  -wrapper.offsetTop;
+      connectBg.style.backgroundSize = `${imgWidth}px auto`;
+      connectBg.style.backgroundPosition = `${posLeft}px ${posTop}px`;
+    },
+  };
+});
 
-    layer.style.transform = transformString;
-    image.style.bottom = '-' + bottomPosition + 'px';
-  });
+blur.set();
 
+window.onresize = function(){
+  blur.set();
 };
 
-window.addEventListener('mousemove', moveLayers);
+var hamburger  = document.querySelector('.hamburger'); // гамбургер на js
+hamburger.onclick = function() {
+  this.classList.toggle('active');
+};
+
+$(document).ready(function(){   
+  // это мягкий скролл от секции к секции
+  $('.arrow-down__link').click( function(){
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length != 0) { 
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); 
+    }
+    return false;
+  });
+
+  $('.hamburger').on('click', function(){ // гамбургер на jquery
+    $(this).toggleClass('active');
+
+    $('.sign-in').click(function(){ //переворачивание плашки
+      $('.info-container').addClass('active');
+    });
+  });
+});
